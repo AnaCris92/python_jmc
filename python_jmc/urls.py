6c7a645ec3216c3455406b4a571ac1f4287cf46b
Cris <criscastillo_2903@outlook.es>
2024-01-08 14:15:43 -0600

    se añadio toda la parte de estilos en la app de empresa, solo falta la parte de las imagenes

--- python_jmc/urls.py
+++ python_jmc/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/',include('index.urls')),
+    path('empresa/',include('empresa.urls')),
 ]
