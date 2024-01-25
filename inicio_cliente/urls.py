15b99e7b93c387638741e34450f98bfdbc13b4cd
Cris <criscastillo_2903@outlook.es>
2024-01-25 11:08:34 -0600

    se agrego la extension de base, segun se reuqeria en cada vista

--- inicio_cliente/urls.py
+++ inicio_cliente/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import contratos_cliente,equipo_cliente,tickets_cliente,pagos_cliente
+
 #vistas
 from . import views
 urlpatterns = [
-    path('',views.inicio_cliente, name='inicio_cliente'),
-    path('equipo_cliente/',views.equipo_cliente, name= 'equipo_cliente'),
-    path('tickets_cliente/',views.tickets_cliente, name= 'tickets_cliente'),
-    path('contratos_cliente/',views.contratos_cliente, name= 'contratos_cliente'),
-    path('pagos_cliente/',views.pagos_cliente, name= 'pagos_cliente'),
+    path('inicio_cliente',views.inicio_cliente, name='inicio_cliente'),
+  
 ]
--- inicio_cliente/views.py
+++ inicio_cliente/views.py
@@ -3,15 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def inicio_cliente(request):
     return render(request,"inicio_cliente.html")
-
-def equipo_cliente(request):
-    return render(request, "equipo_cliente.html")
-
-def tickets_cliente(request):
-    return render(request, "tickets_cliente.html")
-
-def contratos_cliente(request):
-    return render(request, "contratos_cliente.html")
-
-def pagos_cliente(request):
-    return render(request, "pagos_cliente.html")
